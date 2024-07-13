dfb02d4a033db2a24df1354da6b1b90cc6550f57
Derek <xyzhaoup@gmail.com>
2024-07-13 12:44:51 +0000

    07132044

---
+++ code_for_competition/soccer-eater.sh
@@ -0,0 +1,22 @@
+./src/rcssclient role 0 inix -50 iniy 0  &
+sleep 1
+./src/rcssclient role 1 inix -44 iniy 005  &
+sleep 1
+./src/rcssclient role 1 inix -44 iniy -005  &
+sleep 1
+./src/rcssclient role 1 inix -44 iniy 000  &
+sleep 1
+./src/rcssclient role 1 inix -44 iniy -10  &
+sleep 1
+./src/rcssclient role 1 inix -44 iniy 10  &
+sleep 1
+./src/rcssclient role 3 inix -14 iniy -08  &
+sleep 1
+./src/rcssclient role 3 inix -14 iniy 008  &
+sleep 1
+./src/rcssclient role 3 inix -03 iniy -18  &
+sleep 1
+./src/rcssclient role 3 inix -03 iniy 018  &
+sleep 1
+./src/rcssclient role 3 inix -01 iniy 000  &
+sleep 1
